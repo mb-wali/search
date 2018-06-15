89f73c2e3e5b3765fc3b83fe1b40b3d316c1d89c
Ian McEwen <mian@cyverse.org>
2018-06-15 12:41:09 -0700

    Move sort stuff into a new file, use mapstructure to streamline the code

---
+++ data/sort.go
@@ -0,0 +1,46 @@
+package data
+
+import (
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+	"gopkg.in/olivere/elastic.v5"
+)
+
+type sortInput struct {
+	Order string
+	Field string
+}
+
+func extractSort(v map[string]interface{}) ([]elastic.SortInfo, error) {
+	extracted, ok := v["sort"]
+	if !ok {
+		return nil, nil
+	}
+
+	var sorts []sortInput
+
+	err := mapstructure.Decode(extracted, &sorts)
+	if err != nil {
+		return nil, err
+	}
+
+	var ret []elastic.SortInfo
+
+	for _, sort := range sorts {
+		var asc bool
+
+		if sort.Order == "ascending" {
+			asc = true
+		} else if sort.Order == "descending" {
+			asc = false
+
+		} else {
+			return nil, fmt.Errorf("Order of %s was neither ascending nor descending", sort.Order)
+		}
+
+		ret = append(ret, elastic.SortInfo{Ascending: asc, Field: sort.Field})
+	}
+
+	return ret, nil
+}
