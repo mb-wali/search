a8263371b9221829288d7fcd5fa54e183ab7ddb2
Ian McEwen <mian@cyverse.org>
2019-01-14 20:57:35 -0700

    Add the ability to scroll searches with this facade

--- data/data.go
+++ data/data.go
@@ -41,6 +41,17 @@ func init() {
 	tag.Register(qd)
 }
 
+// QueryResponder is a type for managing state in an HTTP handler
+type QueryResponder struct {
+	log    *logrus.Entry
+	output *json.Encoder
+	writer http.ResponseWriter
+	ctx    context.Context
+	cfg    *viper.Viper
+	user   string
+	es     *elasticsearch.Elasticer
+}
+
 // GetAllDocumentationHandler outputs documentation from the QueryDSL instance as JSON.
 func GetAllDocumentationHandler(w http.ResponseWriter, r *http.Request) {
 	docs := make(map[string]interface{})
@@ -59,30 +70,30 @@ func GetAllDocumentationHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(docs)
 }
 
-func logAndOutputString(log *logrus.Entry, err string, out *json.Encoder) {
-	log.Error(err)
-	out.Encode(map[string]string{
+func (r *QueryResponder) logAndOutputString(err string) {
+	r.log.Error(err)
+	r.output.Encode(map[string]string{
 		"error": err,
 	})
 }
 
-func logAndOutputErr(log *logrus.Entry, err error, out *json.Encoder) {
-	log.Error(err)
-	out.Encode(map[string]string{
+func (r *QueryResponder) logAndOutputErr(err error) {
+	r.log.Error(err)
+	r.output.Encode(map[string]string{
 		"error": err.Error(),
 	})
 }
 
 // getUserGroups fetches the user and its groups with qualified names from data-info, returning the list of users, the response raw if it was non-200, and any error. In a non-failing case, only the first returned value will be non-nil.
-func getUserGroups(ctx context.Context, cfg *viper.Viper, user string) ([]string, *http.Response, error) {
+func (r *QueryResponder) getUserGroups() ([]string, *http.Response, error) {
 	// XXX: go 1.9: use url.PathEscape
-	userinfourl := fmt.Sprintf("%s/users/%s/groups?user=%s", cfg.GetString("data_info.base"), user, url.QueryEscape(user))
+	userinfourl := fmt.Sprintf("%s/users/%s/groups?user=%s", r.cfg.GetString("data_info.base"), r.user, url.QueryEscape(r.user))
 	req, err := http.NewRequest("GET", userinfourl, nil)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	req = req.WithContext(ctx)
+	req = req.WithContext(r.ctx)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -116,88 +127,24 @@ func extractInt(v map[string]interface{}, field string, default_val int) int {
 	return int(float)
 }
 
-// GetSearchHandler returns a function which performs searches after translating an input query
-func GetSearchHandler(cfg *viper.Viper, e *elasticsearch.Elasticer, log *logrus.Entry) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
+func (r *QueryResponder) handleScrollId(users []string, sorts []elastic.SortInfo, scrollId string, scroll string) {
+	source, err := r.buildSearchSource(users, sorts)
+	if err != nil {
+		return
+	}
 
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		out := json.NewEncoder(w)
+	res, err := r.es.Scroll().SearchSource(source).ScrollId(scrollId).Scroll(scroll).Do(r.ctx)
+	r.outputSearchResults(res, err)
+}
 
-		queries := r.URL.Query()
-		user := queries.Get("user")
-		if user == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			logAndOutputString(log, "The 'user' query parameter must be provided and non-empty", out)
-			return
-		}
-
-		var v map[string]interface{}
-		err := json.NewDecoder(r.Body).Decode(&v)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			logAndOutputErr(log, err, out)
-			return
-		}
-		query, ok := v["query"]
-		if !ok {
-			w.WriteHeader(http.StatusBadRequest)
-			logAndOutputString(log, "Provided body did not contain a 'query' key", out)
-			return
-		}
-
-		size := extractInt(v, "size", 10)
-		from := extractInt(v, "from", 0)
-
-		sorts, err := extractSort(v)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			logAndOutputErr(log, err, out)
-			return
-		}
-
-		var clauses querydsl.GenericClause
-		qjson, _ := json.Marshal(query)
-		err = json.Unmarshal(qjson, &clauses)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			logAndOutputErr(log, err, out)
-			return
-		}
-
-		users, ur, err := getUserGroups(ctx, cfg, user)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			logAndOutputErr(log, err, out)
-			return
-		}
-		if ur != nil {
-			// passing along the response
-			defer ur.Body.Close()
-			w.WriteHeader(ur.StatusCode)
-			io.Copy(w, ur.Body)
-			return
-		}
-
-		clauses.All = append(clauses.All, &querydsl.GenericClause{Clause: &querydsl.Clause{Type: "permissions", Args: map[string]interface{}{"users": users, "permission": "read", "permission_recurse": true, "exact": true}}})
-
-		// Pass in user and elasticsearch connection
-		translateCtx := context.WithValue(context.WithValue(ctx, "user", users[len(users)-1]), "elasticer", e)
-
-		translated, err := clauses.Translate(translateCtx, qd)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			logAndOutputErr(log, err, out)
-			return
-		}
-		usersJson, err := json.Marshal(users)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			logAndOutputErr(log, err, out)
-			return
-		}
-		permFieldScript := `
+func (r *QueryResponder) buildSearchSource(users []string, sorts []elastic.SortInfo) (*elastic.SearchSource, error) {
+	usersJson, err := json.Marshal(users)
+	if err != nil {
+		r.writer.WriteHeader(http.StatusInternalServerError)
+		r.logAndOutputErr(err)
+		return nil, err
+	}
+	permFieldScript := `
 		String perm = null;
 		for (up in params._source.userPermissions) {
 			for (user in ` + string(usersJson) + `) {
@@ -207,21 +154,160 @@ func GetSearchHandler(cfg *viper.Viper, e *elasticsearch.Elasticer, log *logrus.
 			}
 		}
 		perm`
-		source := elastic.NewSearchSource().FetchSource(true).ScriptField(elastic.NewScriptField("permission", elastic.NewScriptInline(permFieldScript).Lang("painless")))
+	source := elastic.NewSearchSource().FetchSource(true).ScriptField(elastic.NewScriptField("permission", elastic.NewScriptInline(permFieldScript).Lang("painless")))
 
-		for _, sort := range sorts {
-			source = source.SortWithInfo(sort)
-		}
+	for _, sort := range sorts {
+		source = source.SortWithInfo(sort)
+	}
 
-		res, err := e.Search().SearchSource(source).Size(size).From(from).Query(translated).Do(ctx)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			logAndOutputErr(log, err, out)
+	return source, nil
+}
+
+func (r *QueryResponder) outputSearchResults(res *elastic.SearchResult, err error) {
+	if err != nil {
+		r.writer.WriteHeader(http.StatusBadRequest)
+		r.logAndOutputErr(err)
+		return
+	}
+
+	type resp struct {
+		*elastic.SearchHits
+		ScrollId string `json:"scroll_id,omitempty"`
+	}
+
+	response := resp{res.Hits, res.ScrollId}
+
+	r.output.Encode(response)
+}
+
+// GetSearchHandler returns a function which performs searches after translating an input query
+func GetSearchHandler(cfg *viper.Viper, e *elasticsearch.Elasticer, log *logrus.Entry) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		out := json.NewEncoder(w)
+
+		hr := &QueryResponder{es: e, log: log, writer: w, output: out, ctx: ctx, cfg: cfg}
+
+		queries := r.URL.Query()
+		user := queries.Get("user")
+		if user == "" {
+			hr.writer.WriteHeader(http.StatusBadRequest)
+			hr.logAndOutputString("The 'user' query parameter must be provided and non-empty")
 			return
 		}
 
-		out.Encode(res.Hits)
+		hr.user = user
 
+		var v map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&v)
+		if err != nil {
+			hr.writer.WriteHeader(http.StatusBadRequest)
+			hr.logAndOutputErr(err)
+			return
+		}
+		scrollId, hasScrollId := v["scroll_id"]
+		scroll, hasScroll := v["scroll"]
+		query, hasQuery := v["query"]
+		if !hasQuery && !hasScrollId {
+			hr.writer.WriteHeader(http.StatusBadRequest)
+			hr.logAndOutputString("Provided body did not contain a 'query' key nor a 'scroll_id' key")
+			return
+		}
+		if hasQuery && hasScrollId {
+			hr.writer.WriteHeader(http.StatusBadRequest)
+			hr.logAndOutputString("Provided body contained both a 'query' key and a 'scroll_id' key")
+			return
+		}
+
+		if hasScrollId && !hasScroll {
+			hr.writer.WriteHeader(http.StatusBadRequest)
+			hr.logAndOutputString("Provided body has a 'scroll_id' but no 'scroll' (scroll timeout)")
+			return
+		}
+
+		size := extractInt(v, "size", 10)
+		from := extractInt(v, "from", 0)
+
+		sorts, err := extractSort(v)
+		if err != nil {
+			hr.writer.WriteHeader(http.StatusBadRequest)
+			hr.logAndOutputErr(err)
+			return
+		}
+
+		var clauses querydsl.GenericClause
+		qjson, _ := json.Marshal(query)
+		err = json.Unmarshal(qjson, &clauses)
+		if err != nil {
+			hr.writer.WriteHeader(http.StatusBadRequest)
+			hr.logAndOutputErr(err)
+			return
+		}
+
+		users, ur, err := hr.getUserGroups()
+		if err != nil {
+			hr.writer.WriteHeader(http.StatusBadRequest)
+			hr.logAndOutputErr(err)
+			return
+		}
+		if ur != nil {
+			// passing along the response
+			defer ur.Body.Close()
+			hr.writer.WriteHeader(ur.StatusCode)
+			io.Copy(w, ur.Body)
+			return
+		}
+
+		if hasScrollId {
+			scrollIdString, ok := scrollId.(string)
+			if !ok {
+				hr.writer.WriteHeader(http.StatusBadRequest)
+				hr.logAndOutputString("The scroll ID provided could not be converted to a string")
+				return
+			}
+			scrollString, ok := scroll.(string)
+			if !ok {
+				hr.writer.WriteHeader(http.StatusBadRequest)
+				hr.logAndOutputString("The scroll timeout provided could not be converted to a string")
+				return
+			}
+			hr.handleScrollId(users, sorts, scrollIdString, scrollString)
+			return
+		}
+
+		clauses.All = append(clauses.All, &querydsl.GenericClause{Clause: &querydsl.Clause{Type: "permissions", Args: map[string]interface{}{"users": users, "permission": "read", "permission_recurse": true, "exact": true}}})
+
+		// Pass in user and elasticsearch connection
+		translateCtx := context.WithValue(context.WithValue(hr.ctx, "user", users[len(users)-1]), "elasticer", e)
+
+		translated, err := clauses.Translate(translateCtx, qd)
+		if err != nil {
+			hr.writer.WriteHeader(http.StatusBadRequest)
+			hr.logAndOutputErr(err)
+			return
+		}
+		source, err := hr.buildSearchSource(users, sorts)
+		if err != nil {
+			return
+		}
+
+		var res *elastic.SearchResult
+		if hasScroll {
+			scrollString, ok := scroll.(string)
+			if !ok {
+				hr.writer.WriteHeader(http.StatusBadRequest)
+				hr.logAndOutputString("The scroll timeout provided could not be converted to a string")
+				return
+			}
+			res, err = e.Scroll().SearchSource(source).Size(size).Query(translated).Scroll(scrollString).Do(hr.ctx)
+		} else {
+			res, err = e.Search().SearchSource(source).Size(size).From(from).Query(translated).Do(hr.ctx)
+		}
+		hr.outputSearchResults(res, err)
 	}
 }
 
